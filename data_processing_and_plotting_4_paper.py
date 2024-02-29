796a07f303b7d10aca007cd90856c5b625a2884a
prachisharma21 <prachi16490@gmail.com>
2024-02-29 10:42:48 +0100

    change var names

--- data_processing_and_plotting_4_paper.py
+++ data_processing_and_plotting_4_paper.py
@@ -12,40 +12,39 @@ dataAg = np.loadtxt("./bandstructure_unfolded-path-SC333-k444-Ag-ecut100.txt")
 
 # load data of DOS, partial DOS and projected DOS on each atom
 # For the Fe doping
-enFe, dosFe, idosFe = np.loadtxt('./data_DOS_FeSC333_k444_e100/PdCoO2_Fe_doping_dos.dat', unpack=True)
-energydosFe, edosFe, pdosFe =  np.loadtxt('./data_DOS_FeSC333_k444_e100/PdCoO2_Fe_doping_pdos.dat.pdos_tot',unpack=True)
-ePdFe, PdedosFe = np.loadtxt('./data_DOS_FeSC333_k444_e100/Pd_PDOS.dat', unpack=True)
-eCoFe, CoedosFe = np.loadtxt('./data_DOS_FeSC333_k444_e100/Co_PDOS.dat', unpack=True)
-eOFe, OedosFe = np.loadtxt('./data_DOS_FeSC333_k444_e100/O_PDOS.dat', unpack=True)
-eFeFe, FeedosFe = np.loadtxt('./data_DOS_FeSC333_k444_e100/Fe_PDOS.dat', unpack=True)
+en_w_Fe, dos_w_Fe, idos_w_Fe = np.loadtxt('./data_DOS_FeSC333_k444_e100/PdCoO2_Fe_doping_dos.dat', unpack=True)
+energydos_w_Fe, edos_w_Fe, pdos_w_Fe =  np.loadtxt('./data_DOS_FeSC333_k444_e100/PdCoO2_Fe_doping_pdos.dat.pdos_tot',unpack=True)
+ePd_w_Fe, Pdedos_w_Fe = np.loadtxt('./data_DOS_FeSC333_k444_e100/Pd_PDOS.dat', unpack=True)
+eCo_w_Fe, Coedos_w_Fe = np.loadtxt('./data_DOS_FeSC333_k444_e100/Co_PDOS.dat', unpack=True)
+eO_w_Fe, Oedos_w_Fe = np.loadtxt('./data_DOS_FeSC333_k444_e100/O_PDOS.dat', unpack=True)
+edop_w_Fe, dopedos_w_Fe = np.loadtxt('./data_DOS_FeSC333_k444_e100/Fe_PDOS.dat', unpack=True)
 
 # For Pt doping
-enPt, dosPt, idosPt = np.loadtxt('./data_DOS_PtSC333_k444_e100/PdCoO2_Pt_doping_dos.dat', unpack=True)
-energydosPt, edosPt, pdosPt =  np.loadtxt('./data_DOS_PtSC333_k444_e100/PdCoO2_Pt_doping_pdos.dat.pdos_tot',unpack=True)
-ePdPt, PdedosPt = np.loadtxt('./data_DOS_PtSC333_k444_e100/Pd_PDOS.dat', unpack=True)
-eCoPt, CoedosPt = np.loadtxt('./data_DOS_PtSC333_k444_e100/Co_PDOS.dat', unpack=True)
-eOPt, OedosPt = np.loadtxt('./data_DOS_PtSC333_k444_e100/O_PDOS.dat', unpack=True)
-eFePt, FeedosPt = np.loadtxt('./data_DOS_PtSC333_k444_e100/Pt_PDOS.dat', unpack=True)
+en_w_Pt, dos_w_Pt, idos_w_Pt = np.loadtxt('./data_DOS_PtSC333_k444_e100/PdCoO2_Pt_doping_dos.dat', unpack=True)
+energydos_w_Pt, edos_w_Pt, pdos_w_Pt =  np.loadtxt('./data_DOS_PtSC333_k444_e100/PdCoO2_Pt_doping_pdos.dat.pdos_tot',unpack=True)
+ePd_w_Pt, Pdedos_w_Pt = np.loadtxt('./data_DOS_PtSC333_k444_e100/Pd_PDOS.dat', unpack=True)
+eCo_w_Pt, Coedos_w_Pt = np.loadtxt('./data_DOS_PtSC333_k444_e100/Co_PDOS.dat', unpack=True)
+eO_w_Pt, Oedos_w_Pt = np.loadtxt('./data_DOS_PtSC333_k444_e100/O_PDOS.dat', unpack=True)
+edop_w_Pt, dopedos_w_Pt = np.loadtxt('./data_DOS_PtSC333_k444_e100/Pt_PDOS.dat', unpack=True)
 
 # For Al doping
-enAl, dosAl, idosAl = np.loadtxt('./data_DOS_AlSC333_k444_e100/PdCoO2_Al_doping_dos.dat', unpack=True)
-energydosAl, edosAl, pdosAl =  np.loadtxt('./data_DOS_AlSC333_k444_e100/PdCoO2_Al_doping_pdos.dat.pdos_tot',unpack=True)
-ePdAl, PdedosAl = np.loadtxt('./data_DOS_AlSC333_k444_e100/Pd_PDOS.dat',unpack=True)
-eCoAl, CoedosAl = np.loadtxt('./data_DOS_AlSC333_k444_e100/Co_PDOS.dat',unpack=True)
-eOAl, OedosAl = np.loadtxt('./data_DOS_AlSC333_k444_e100/O_PDOS.dat',unpack=True)
-eFeAl, FeedosAl = np.loadtxt('./data_DOS_AlSC333_k444_e100/Al_PDOS.dat',unpack=True)
+en_w_Al, dos_w_Al, idos_w_Al = np.loadtxt('./data_DOS_AlSC333_k444_e100/PdCoO2_Al_doping_dos.dat', unpack=True)
+energydos_w_Al, edos_w_Al, pdos_w_Al =  np.loadtxt('./data_DOS_AlSC333_k444_e100/PdCoO2_Al_doping_pdos.dat.pdos_tot',unpack=True)
+ePd_w_Al, Pdedos_w_Al = np.loadtxt('./data_DOS_AlSC333_k444_e100/Pd_PDOS.dat',unpack=True)
+eCo_w_Al, Coedos_w_Al = np.loadtxt('./data_DOS_AlSC333_k444_e100/Co_PDOS.dat',unpack=True)
+eO_w_Al, Oedos_w_Al = np.loadtxt('./data_DOS_AlSC333_k444_e100/O_PDOS.dat',unpack=True)
+edop_w_Al, dopedos_w_Al = np.loadtxt('./data_DOS_AlSC333_k444_e100/Al_PDOS.dat',unpack=True)
 
 # For Ag doping
-enAg, dosAg, idosAg = np.loadtxt('./data_DOS_AgSC333_k444_e100/PdCoO2_Ag_doping_dos.dat', unpack=True)
-energydosAg, edosAg, pdosAg =  np.loadtxt('./data_DOS_AgSC333_k444_e100/PdCoO2_Ag_doping_pdos.dat.pdos_tot',unpack=True)
-ePdAg, PdedosAg = np.loadtxt('./data_DOS_AgSC333_k444_e100/Pd_PDOS.dat', unpack=True)
-eCoAg, CoedosAg = np.loadtxt('./data_DOS_AgSC333_k444_e100/Co_PDOS.dat', unpack=True)
-eOAg, OedosAg = np.loadtxt('./data_DOS_AgSC333_k444_e100/O_PDOS.dat', unpack=True)
-eFeAg, FeedosAg = np.loadtxt('./data_DOS_AgSC333_k444_e100/Ag_PDOS.dat', unpack=True)
+en_w_Ag, dos_w_Ag, idos_w_Ag = np.loadtxt('./data_DOS_AgSC333_k444_e100/PdCoO2_Ag_doping_dos.dat', unpack=True)
+energydos_w_Ag, edos_w_Ag, pdos_w_Ag =  np.loadtxt('./data_DOS_AgSC333_k444_e100/PdCoO2_Ag_doping_pdos.dat.pdos_tot',unpack=True)
+ePd_w_Ag, Pdedos_w_Ag = np.loadtxt('./data_DOS_AgSC333_k444_e100/Pd_PDOS.dat', unpack=True)
+eCo_w_Ag, Coedos_w_Ag = np.loadtxt('./data_DOS_AgSC333_k444_e100/Co_PDOS.dat', unpack=True)
+eO_w_Ag, Oedos_w_Ag = np.loadtxt('./data_DOS_AgSC333_k444_e100/O_PDOS.dat', unpack=True)
+edop_w_Ag, dopedos_w_Ag = np.loadtxt('./data_DOS_AgSC333_k444_e100/Ag_PDOS.dat', unpack=True)
 
 
 # function defined to calculate the density from the band structure data 
-
 def calc_density_1(data,Ebbd , dE, efermi, smearing):
     """ This function calculated the density from the bandstructure data
     Input:
@@ -91,29 +90,30 @@ Energyboundary=1.5
 dE=5000
 
 # calculating density for each specific dopant data file 
+
 # For Fe doping
-densityFe, energyFe = calc_density_1(dataFe, Energyboundary, dE, EfermiFe, smearing)
-k1Fe = np.unique(dataFe[:, 0])/0.34140624870604636  # the number is rescaling factor from supercell to primitive cell 
-E1Fe = energyFe-EfermiFe
-k1Fe,E1Fe = np.meshgrid(k1Fe,E1Fe)
+density_w_Fe, energy_w_Fe = calc_density_1(dataFe, Energyboundary, dE, EfermiFe, smearing)
+k1_w_Fe = np.unique(dataFe[:, 0])/0.34140624870604636  # the number is rescaling factor from supercell to primitive cell 
+E1_w_Fe = energy_w_Fe-EfermiFe
+k1_w_Fe,E1_w_Fe = np.meshgrid(k1_w_Fe,E1_w_Fe)
 
 # For Pt doping
-densityPt, energyPt = calc_density_1(dataPt, Energyboundary, dE, EfermiPt, smearing) 
-k1Pt=np.unique(dataPt[:, 0])/0.34140624870604636 
-E1Pt=energyPt-EfermiPt
-k1Pt,E1Pt=np.meshgrid(k1Pt,E1Pt)
+density_w_Pt, energy_w_Pt = calc_density_1(dataPt, Energyboundary, dE, EfermiPt, smearing) 
+k1_w_Pt=np.unique(dataPt[:, 0])/0.34140624870604636 
+E1_w_Pt=energy_w_Pt-EfermiPt
+k1_w_Pt,E1_w_Pt=np.meshgrid(k1_w_Pt,E1_w_Pt)
 
 # For Al doping
-densityAl, energyAl =  calc_density_1(dataAl, Energyboundary, dE, EfermiAl, smearing)
-k1Al=np.unique(dataAl[:, 0])/0.34140624870604636 
-E1Al=energyAl-EfermiAl
-k1Al,E1Al=np.meshgrid(k1Al,E1Al)
+density_w_Al, energy_w_Al =  calc_density_1(dataAl, Energyboundary, dE, EfermiAl, smearing)
+k1_w_Al=np.unique(dataAl[:, 0])/0.34140624870604636 
+E1_w_Al=energy_w_Al-EfermiAl
+k1_w_Al,E1_w_Al=np.meshgrid(k1_w_Al,E1_w_Al)
 
 # For Ag doping
-densityAg, energyAg =  calc_density_1(dataAg, Energyboundary, dE, EfermiAg, smearing)
-k1Ag=np.unique(dataAg[:, 0])/0.34140624870604636 
-E1Ag=energyAg-EfermiAg
-k1Ag,E1Ag=np.meshgrid(k1Ag,E1Ag)
+density_w_Ag, energy_w_Ag =  calc_density_1(dataAg, Energyboundary, dE, EfermiAg, smearing)
+k1_w_Ag=np.unique(dataAg[:, 0])/0.34140624870604636 
+E1_w_Ag=energy_w_Ag-EfermiAg
+k1_w_Ag,E1_w_Ag=np.meshgrid(k1_w_Ag,E1_w_Ag)
 
 
 # The following is to set the size of the figure and gridspec_kw is to adjust the size of the plots in the figure.
@@ -127,8 +127,8 @@ fig, axs = plt.subplots(4, 2,figsize=(10,8),gridspec_kw={'width_ratios':[1,0.3]}
 ##########################################################################################################
 
 # density plot for Fe doping
-pcm00 =axs[2, 0].pcolormesh(k1Fe,E1Fe,densityFe.T, cmap='magma', shading='auto', 
-                            norm=colors.LogNorm(vmin= np.min(densityFe.T)+0.01, vmax= np.max(densityFe.T)))
+pcm00 =axs[2, 0].pcolormesh(k1_w_Fe,E1_w_Fe,density_w_Fe.T, cmap='magma', shading='auto', 
+                            norm=colors.LogNorm(vmin= np.min(density_w_Fe.T)+0.01, vmax= np.max(density_w_Fe.T)))
 axs[2,0].spines['top'].set_linewidth(1.5)
 axs[2,0].spines['right'].set_linewidth(1.5)
 axs[2,0].spines['bottom'].set_linewidth(1.5)
@@ -146,11 +146,11 @@ axs[2,0].axvline(2.5162, linewidth=1, color='w', alpha=0.8, linestyle = '-')
 axs[2,0].axvline(3.8095, linewidth=1, color='w', alpha=0.8, linestyle = '-')
 
 # DOS plots for Fe doping
-axs[2,1].plot(dosFe/27,enFe-EfermiFe,linewidth = 1, color="k", label="Total")
-axs[2,1].plot(PdedosFe/27,ePdFe-EfermiFe,linewidth = 1, color="darkgoldenrod", label="Pd")
-axs[2,1].plot(CoedosFe/26,eCoFe-EfermiFe,linewidth = 1, color="royalblue",label= "Co")
-axs[2,1].plot(OedosFe/54,eOFe-EfermiFe,linewidth = 1, color="r", label = "O")
-axs[2,1].plot(FeedosFe,eFeFe-EfermiFe,linewidth = 1, color="aqua", label = "Fe")
+axs[2,1].plot(dos_w_Fe/27,en_w_Fe-EfermiFe,linewidth = 1, color="k", label="Total")
+axs[2,1].plot(Pdedos_w_Fe/27,ePd_w_Fe-EfermiFe,linewidth = 1, color="darkgoldenrod", label="Pd")
+axs[2,1].plot(Coedos_w_Fe/26,eCo_w_Fe-EfermiFe,linewidth = 1, color="royalblue",label= "Co")
+axs[2,1].plot(Oedos_w_Fe/54,eO_w_Fe-EfermiFe,linewidth = 1, color="r", label = "O")
+axs[2,1].plot(dopedos_w_Fe,edop_w_Fe-EfermiFe,linewidth = 1, color="aqua", label = "Fe")
 axs[2,1].spines['top'].set_linewidth(1.5)
 axs[2,1].spines['right'].set_linewidth(1.5)
 axs[2,1].spines['bottom'].set_linewidth(1.5)
@@ -168,8 +168,8 @@ axs[2,1].legend(loc='upper right',fontsize="8",prop=font,frameon=False,facecolor
 ###############################################################################################################
 
 # density plot for Pt doping
-pcm10 = axs[0, 0].pcolormesh(k1Pt,E1Pt,densityPt.T, cmap='magma', shading='auto', 
-                             norm=colors.LogNorm(vmin= np.min(densityPt.T)+0.01, vmax= np.max(densityPt.T)))
+pcm10 = axs[0, 0].pcolormesh(k1_w_Pt,E1_w_Pt,density_w_Pt.T, cmap='magma', shading='auto', 
+                             norm=colors.LogNorm(vmin= np.min(density_w_Pt.T)+0.01, vmax= np.max(density_w_Pt.T)))
 axs[0,0].spines['top'].set_linewidth(1.5)
 axs[0,0].spines['right'].set_linewidth(1.5)
 axs[0,0].spines['bottom'].set_linewidth(1.5)
@@ -187,11 +187,11 @@ axs[0,0].axvline(2.5162, linewidth=1, color='w', alpha=0.8, linestyle = '-')
 axs[0,0].axvline(3.8095, linewidth=1, color='w', alpha=0.8, linestyle = '-')
 
 # DOS for Pt doping
-axs[0,1].plot(dosPt/27,enPt-EfermiPt,linewidth = 1, color="k", label="Total")
-axs[0,1].plot(PdedosPt/26,ePdPt-EfermiPt,linewidth = 1, color="darkgoldenrod", label="Pd")
-axs[0,1].plot(CoedosPt/27,eCoPt-EfermiPt,linewidth = 1, color="royalblue",label= "Co")
-axs[0,1].plot(OedosPt/54,eOPt-EfermiPt,linewidth = 1, color="r", label = "O")
-axs[0,1].plot(FeedosPt,eFePt-EfermiPt,linewidth = 1, color="aqua", label = "Pt")
+axs[0,1].plot(dos_w_Pt/27,en_w_Pt-EfermiPt,linewidth = 1, color="k", label="Total")
+axs[0,1].plot(Pdedos_w_Pt/26,ePd_w_Pt-EfermiPt,linewidth = 1, color="darkgoldenrod", label="Pd")
+axs[0,1].plot(Coedos_w_Pt/27,eCo_w_Pt-EfermiPt,linewidth = 1, color="royalblue",label= "Co")
+axs[0,1].plot(Oedos_w_Pt/54,eO_w_Pt-EfermiPt,linewidth = 1, color="r", label = "O")
+axs[0,1].plot(dopedos_w_Pt,edop_w_Pt-EfermiPt,linewidth = 1, color="aqua", label = "Pt")
 axs[0,1].spines['top'].set_linewidth(1.5)
 axs[0,1].spines['right'].set_linewidth(1.5)
 axs[0,1].spines['bottom'].set_linewidth(1.5)
@@ -208,8 +208,8 @@ axs[0,1].legend(loc='upper right',fontsize="8",prop=font,frameon=False,facecolor
 ##############################################################################################################
 
 # Density plot for Al doping
-pcm20 = axs[3, 0].pcolormesh(k1Al,E1Al,densityAl.T, cmap='magma', shading='auto', 
-                             norm=colors.LogNorm(vmin= np.min(densityAl.T)+0.01, vmax= np.max(densityAl.T)))
+pcm20 = axs[3, 0].pcolormesh(k1_w_Al,E1_w_Al,density_w_Al.T, cmap='magma', shading='auto', 
+                             norm=colors.LogNorm(vmin= np.min(density_w_Al.T)+0.01, vmax= np.max(density_w_Al.T)))
 axs[3,0].spines['top'].set_linewidth(1.5)
 axs[3,0].spines['right'].set_linewidth(1.5)
 axs[3,0].spines['bottom'].set_linewidth(1.5)
@@ -229,11 +229,11 @@ axs[3,0].axvline(3.8095, linewidth=1, color='w', alpha=0.8, linestyle = '-')
 
 # DOS for Al doping
 # here I've divided the DOS for the number of the respective atoms in the Supercell. 
-axs[3,1].plot(dosAl/27,enAl-EfermiAl,linewidth = 1, color="k", label="Total") #marker=".", markersize=4, 
-axs[3,1].plot(PdedosAl/27,ePdAl-EfermiAl,linewidth = 1, color="darkgoldenrod", label="Pd")
-axs[3,1].plot(CoedosAl/26,eCoAl-EfermiAl,linewidth = 1, color="royalblue",label= "Co")
-axs[3,1].plot(OedosAl/54,eOAl-EfermiAl,linewidth = 1, color="r", label = "O")
-axs[3,1].plot(FeedosAl,eFeAl-EfermiAl,linewidth = 1, color="aqua", label = "Al")
+axs[3,1].plot(dos_w_Al/27,en_w_Al-EfermiAl,linewidth = 1, color="k", label="Total") #marker=".", markersize=4, 
+axs[3,1].plot(Pdedos_w_Al/27,ePd_w_Al-EfermiAl,linewidth = 1, color="darkgoldenrod", label="Pd")
+axs[3,1].plot(Coedos_w_Al/26,eCo_w_Al-EfermiAl,linewidth = 1, color="royalblue",label= "Co")
+axs[3,1].plot(Oedos_w_Al/54,eO_w_Al-EfermiAl,linewidth = 1, color="r", label = "O")
+axs[3,1].plot(dopedos_w_Al,edop_w_Al-EfermiAl,linewidth = 1, color="aqua", label = "Al")
 axs[3,1].spines['top'].set_linewidth(1.5)
 axs[3,1].spines['right'].set_linewidth(1.5)
 axs[3,1].spines['bottom'].set_linewidth(1.5)
@@ -252,8 +252,8 @@ axs[3,1].legend(loc='upper right',fontsize="8",prop=font,frameon=False,facecolor
 #############################################################################################################
 
 # Density plot for Ag doping
-pcm30 = axs[1, 0].pcolormesh(k1Ag,E1Ag,densityAg.T, cmap='magma', shading='auto', 
-                             norm=colors.LogNorm(vmin= np.min(densityAg.T)+0.01, vmax= np.max(densityAg.T)))
+pcm30 = axs[1, 0].pcolormesh(k1_w_Ag,E1_w_Ag,density_w_Ag.T, cmap='magma', shading='auto', 
+                             norm=colors.LogNorm(vmin= np.min(density_w_Ag.T)+0.01, vmax= np.max(density_w_Ag.T)))
 axs[1,0].spines['top'].set_linewidth(1.5)
 axs[1,0].spines['right'].set_linewidth(1.5)
 axs[1,0].spines['bottom'].set_linewidth(1.5)
@@ -274,11 +274,11 @@ axs[1,0].axvline(3.8095, linewidth=1, color='w', alpha=0.8, linestyle = '-')
 
 # DOS for Ag doping
 # here we've divided the DOS for the number of the respective atoms in the Supercell. 
-axs[1,1].plot(dosAg/27,enAg-EfermiAg,linewidth = 1, color="k", label="Total") #marker=".", markersize=4, 
-axs[1,1].plot(PdedosAg/26,ePdAg-EfermiAg,linewidth = 1, color="darkgoldenrod", label="Pd")
-axs[1,1].plot(CoedosAg/27,eCoAg-EfermiAg,linewidth = 1, color="royalblue",label= "Co")
-axs[1,1].plot(OedosAg/54,eOAg-EfermiAg,linewidth = 1, color="r", label = "O")
-axs[1,1].plot(FeedosAg,eFeAg-EfermiAg,linewidth = 1, color="aqua", label = "Ag")
+axs[1,1].plot(dos_w_Ag/27,en_w_Ag-EfermiAg,linewidth = 1, color="k", label="Total") #marker=".", markersize=4, 
+axs[1,1].plot(Pdedos_w_Ag/26,ePd_w_Ag-EfermiAg,linewidth = 1, color="darkgoldenrod", label="Pd")
+axs[1,1].plot(Coedos_w_Ag/27,eCo_w_Ag-EfermiAg,linewidth = 1, color="royalblue",label= "Co")
+axs[1,1].plot(Oedos_w_Ag/54,eO_w_Ag-EfermiAg,linewidth = 1, color="r", label = "O")
+axs[1,1].plot(dopedos_w_Ag,edop_w_Ag-EfermiAg,linewidth = 1, color="aqua", label = "Ag")
 axs[1,1].spines['top'].set_linewidth(1.5)
 axs[1,1].spines['right'].set_linewidth(1.5)
 axs[1,1].spines['bottom'].set_linewidth(1.5)
@@ -293,7 +293,7 @@ axs[1,1].set_xlabel('DOS (states/eV atom)',fontsize=14,fontname='Arial')
 axs[1,1].axhline(0, linewidth=1, color='k', alpha=0.8, linestyle = '-')
 axs[1,1].legend(loc='upper right',fontsize="8",prop=font,frameon=False,facecolor='none',borderpad=0.2,labelspacing=0.3,handlelength=1,handletextpad=0.4,borderaxespad=0.2,bbox_to_anchor=(1,0.6))
 
-###########################################################################################
+#######################################################################################################
 
 # to adjust the spacing between the three plots both as width and height. 
 plt.subplots_adjust(wspace=0.04, hspace=0.15)
